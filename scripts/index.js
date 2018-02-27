7a615c02592728e5b1b9199029d893d7e9302078
Joshua Hutchinson <jthutch94@gmail.com>
2018-02-27 18:20:25 -0500

    working-on-generateVideoItemHtml

--- scripts/index.js
+++ scripts/index.js
@@ -15,17 +15,17 @@ const BASE_URL = 'https://www.googleapis.com/youtube/v3/search';
 // 2. Use `searchTerm` to construct the right query object based on the Youtube API docs
 // 3. Make a getJSON call using the query object and sending the provided callback in as the last argument
 // TEST IT! Execute this function and console log the results inside the callback.
-const fetchVideos = function(searchTerm, callback) {
+const fetchVideos = function (searchTerm, callback) {
   const query = {
-    part:'snippet', 
+    part: 'snippet',
     key: API_KEY,
-    q: `${searchTerm} in:name`, 
+    q: `${searchTerm} in:name`,
     per_page: 5
   };
   let fetchedVideo = $.getJSON(BASE_URL, query, callback);
 };
-fetchVideos('Michael Jackson', function(data){
-  console.log(decorateResponse(data));
+fetchVideos('Michael Jackson', function (data) {
+  //console.log(decorateResponse(data));
 });
 // TASK:
 // 1. Create a `decorateResponse` function that receives the Youtube API response
@@ -35,61 +35,63 @@ fetchVideos('Michael Jackson', function(data){
 // WILL have to dig into several nested properties!
 // TEST IT! Grab an example API response and send it into the function - make sure
 // you get back the object you want.
-const decorateResponse = function(fetchedVideo) {
+const decorateResponse = function (fetchedVideo) {
   const myArray = fetchedVideo.items.map(item => {
     return {
       id: item.id.videoId,
       title: item.snippet.title,
-      thumbnail: item.snippet.thumbnails.default
+      thumbnail: item.snippet.thumbnails.default.url
     };
   });
   return myArray;
 };
 
+// TASK:
+// 1. Create a `generateVideoItemHtml` function that receives the decorated object
+// 2. Using the object, return an HTML string containing all the expected data
+// TEST IT!
+const generateVideoItemHtml = function (myArray) {
+  console.log( `<span>${myArray.id}</span>
+            <span>${myArray.title}</span>
+            <img src='${myArray.thumbnail}'>
+        </li>`);
+};
 
+generateVideoItemHtml();
 
-
-  // TASK:
-  // 1. Create a `generateVideoItemHtml` function that receives the decorated object
-  // 2. Using the object, return an HTML string containing all the expected data
-  // TEST IT!
-const generateVideoItemHtml = function(video) {
+// TASK:
+// 1. Create a `addVideosToStore` function that receives an array of decorated video 
+// objects and sets the array as the value held in store.items
+// TEST IT!
+const addVideosToStore = function (videos) {
 
 };
 
-  // TASK:
-  // 1. Create a `addVideosToStore` function that receives an array of decorated video 
-  // objects and sets the array as the value held in store.items
-  // TEST IT!
-const addVideosToStore = function(videos) {
+// TASK:
+// 1. Create a `render` function
+// 2. Map through `store.videos`, sending each `video` through your `generateVideoItemHtml`
+// 3. Add your array of DOM elements to the appropriate DOM element
+// TEST IT!
+const render = function () {
 
 };
 
-  // TASK:
-  // 1. Create a `render` function
-  // 2. Map through `store.videos`, sending each `video` through your `generateVideoItemHtml`
-  // 3. Add your array of DOM elements to the appropriate DOM element
-  // TEST IT!
-const render = function() {
+// TASK:
+// 1. Create a `handleFormSubmit` function that adds an event listener to the form
+// 2. The listener should:
+//   a) Prevent default event
+//   b) Retrieve the search input from the DOM
+//   c) Clear the search input field
+//   d) Invoke the `fetchVideos` function, sending in the search value
+//   e) Inside the callback, send the API response through the `decorateResponse` function
+//   f) Inside the callback, add the decorated response into your store using the `addVideosToStore` function
+//   g) Inside the callback, run the `render` function 
+// TEST IT!
+const handleFormSubmit = function () {
 
 };
 
-  // TASK:
-  // 1. Create a `handleFormSubmit` function that adds an event listener to the form
-  // 2. The listener should:
-  //   a) Prevent default event
-  //   b) Retrieve the search input from the DOM
-  //   c) Clear the search input field
-  //   d) Invoke the `fetchVideos` function, sending in the search value
-  //   e) Inside the callback, send the API response through the `decorateResponse` function
-  //   f) Inside the callback, add the decorated response into your store using the `addVideosToStore` function
-  //   g) Inside the callback, run the `render` function 
-  // TEST IT!
-const handleFormSubmit = function() {
-
-};
-
-  // When DOM is ready:
+// When DOM is ready:
 $(function () {
   // TASK:
   // 1. Run `handleFormSubmit` to bind the event listener to the DOM
